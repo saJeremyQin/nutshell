0a2430144699123fdda62aa4f3321a6f11e13f18
saJeremyQin <343220868@qq.com>
2022-12-21 18:30:23 +1030

    use JereButton

--- screens/TableNumScreen.js
+++ screens/TableNumScreen.js
@@ -4,8 +4,6 @@ import {
     Image,
     Text,
     TextInput,
-    Button,
-    ImageBackground
 } from "react-native";
 
 import { useContext,useState } from "react";
@@ -19,8 +17,9 @@ export default function TableNumScreen({ navigation }) {
 
     const { tableNumData, setTableNumData } = useContext(TableNumContex);
 
+    // control the status of JereButton
     const onPress = () => {
-
+        setIsSubmitted(!isSubmitted);
     }
 
     return (
@@ -32,45 +31,80 @@ export default function TableNumScreen({ navigation }) {
             />
             <TextInput
                 placeholder='Please input table number!' 
-                style={styles.textInput} 
+                style={styles.input} 
                 keyboardType='numeric'
                 onChangeText={(value)=>{setTableNumData(value)}}           
             />
-            <View style={styles.buttonContainer}>
+            {/* <View style={styles.buttonContainer}>
                 <View style={styles.buttonView}>
                     <Button title='Set Table Number' onPress={onPress()} color="#5e0acc" />
                 </View>
-            </View> 
+            </View>  */}
+            <JereButton 
+                onPress={onPress} 
+                title={isSubmitted ? 'Clear' : 'Set Number'} 
+                color={"#5e0acc"}
+            />
+            {
+                isSubmitted ?
+                (
+                    <>
+                        <Text style={[styles.text]}>
+                            Current Table Number is {tableNumData}
+                        </Text>
+                        <Image style={styles.dimage}
+                            source={require("../assets/done.png")}
+                            resizeMode="stretch"
+                        /> 
+                    </>
+                ) : (
+                    <>
+                    </>
+                )
+            }
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-  body:{
-    flex:1,
-    alignItems:"center"
-  },
-  textInput: {
-    width:"90%",
-    borderWidth:1,
-    borderColor:"#e4d0ff",
-    borderRadius:6,
-    backgroundColor:"#e4d0ff",
-    color:"#120438",
-    padding:5
-},
-buttonContainer: {
-    flexDirection:"row",
-    padding:8,
-},
-image: {
-    width:300,
-    height:300,
-    padding:20
-},
-buttonView:{
-    width:"40%",
-    marginHorizontal:8
-}
-    
+    body:{
+        flex:1,
+        alignItems:"center"
+    },
+    input:{
+        width:250,
+        height:40,
+        borderWidth:1,
+        borderColor:"#555",
+        borderRadius:5,
+        textAlign:"center",
+        fontSize: 20,
+        marginTop:30,
+        marginBottom:10
+    },
+    textInput: {
+        width:"90%",
+        borderWidth:1,
+        borderColor:"#e4d0ff",
+        borderRadius:6,
+        backgroundColor:"#e4d0ff",
+        color:"#120438",
+        padding:5
+    },
+    text:{
+        fontSize:24,
+        color:"black",
+        margin:5,
+        textAlign:"center"
+    },   
+    image: {
+        width:300,
+        height:300,
+        padding:20
+    },
+    dimage:{
+        width:80,
+        height:80,
+        marginTop:8
+    }    
 })
